605c171d405883a659e5dc06b60eef1e2db06292
Benoit Hediard <ben@agorapulse.com>
2014-05-21 12:27:27 +0200

    Fix plugin documentation link [skip ci]

--- CoverallsGrailsPlugin.groovy
+++ CoverallsGrailsPlugin.groovy
@@ -12,7 +12,7 @@ class CoverallsGrailsPlugin {
     def description = '''The Coveralls Plugin allows you to send Grails code coverage report to Coveralls.
 Coveralls works with your continuous integration server to give you test coverage history and statistics.'''
 
-    def documentation = "http://grails.org/plugin/grails-coveralls"
+    def documentation = "http://grails.org/plugin/coveralls"
     def license = "APACHE"
     def organization = [ name: "AgoraPulse", url: "http://www.agorapulse.com/" ]
     def issueManagement = [ system: "github", url: "https://github.com/agorapulse/grails-coveralls/issues" ]
